1c2a8e17cce192600f292a93e4fadf38b87273b2
Kevin <kevin@example.com>
2022-08-05 16:22:28 -0700

    add read-only dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,7 @@
 ((c-mode
   (indent-tabs-mode . nil)
   (c-file-style . "bsd")
-  (c-basic-offset . 2)))
+  (c-basic-offset . 2))
+ (nil . ((eval . (when buffer-file-name
+                   (setq-local view-no-disable-on-exit t)
+                   (view-mode-enter))))))
